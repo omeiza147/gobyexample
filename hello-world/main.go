2ebd896158ca1cf17c2cff41c98c2f8340bd6825
G <sampark201@icloud.com>
2026-09-06 14:59:14 -0700

    sample go program using fmt

---
+++ hello-world/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Greatness from Small Beginnings") // this is a single line comment
+}
+
+/*
+this is a multi line comment
+Line 1: every program is part of package
+Line 3: import  statement lets us import packages to use into our program
+package fmt in go  does a series of input ouput operations. read more on it
+Line 5: fmt.Println is a function from package fmt to print something to the terminal
+*/
